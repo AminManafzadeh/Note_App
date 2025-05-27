ef601dc23ba247b0d67577b08e30e083643560df
Amin Manafzadeh <amin.mah1375@gmail.com>
2025-05-27 06:01:02 -0700

    add noteapp project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="container max-w-[1080px] mx-auto text-center">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
