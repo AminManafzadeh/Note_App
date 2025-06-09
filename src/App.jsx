aadb681680600400923a13cd8d73a4a177bc64fb
Amin Manafzadeh <amin.mah1375@gmail.com>
2025-06-09 05:58:54 -0700

    add useReduce

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,50 @@ import AddNewNote from "./components/AddNewNote";
 import Header from "./components/Header";
 import NoteList from "./components/NoteList";
 import NoteStatus from "./components/NoteStatus";
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+function noteReducer(state, { type, payload }) {
+  switch (type) {
+    case "addNotes": {
+      return [...state, payload];
+    }
+    case "deleteNote": {
+      return [...state].filter((note) => note.id !== payload);
+    }
+    case "completedNote": {
+      return [...state].map((note) =>
+        note.id === payload ? { ...note, completed: !note.completed } : note
+      );
+    }
+
+    default:
+      return state;
+  }
+}
 
 function App() {
-  const [notes, setNotes] = useState([]);
+  // const [notes, setNotes] = useState([]);
+  const [notes, dispatch] = useReducer(noteReducer, []);
   const [sortBy, setSortBy] = useState("latest");
 
   const handleAddNote = (newNote) => {
-    setNotes([...notes, newNote]);
+    // setNotes([...notes, newNote]);
+    dispatch({ type: "addNotes", payload: newNote });
   };
 
   const handleDelete = (id) => {
-    const filteredNotes = notes?.filter((note) => note.id !== id);
-    setNotes(filteredNotes);
+    // const filteredNotes = notes?.filter((note) => note.id !== id);
+    // setNotes(filteredNotes);
+    dispatch({ type: "deleteNote", payload: id });
   };
 
   const handleCompleted = (e) => {
     const noteId = Number(e.target.value);
-    const completedNotes = notes.map((note) =>
-      note.id === noteId ? { ...note, completed: !note.completed } : note
-    );
-    setNotes(completedNotes);
+    // const completedNotes = notes.map((note) =>
+    //   note.id === noteId ? { ...note, completed: !note.completed } : note
+    // );
+    // setNotes(completedNotes);
+    dispatch({ type: "completedNote", payload: noteId });
   };
 
   return (
